fae4975c41796cc554fa545a791244c982ca9ac3
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2025-06-03 09:06:17 +0700

    -

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu
+LABEL maintainer="Fahrudin Hariadi<fahrudin.hariadi@gmail.com>"
+ARG CONTAINER_DOMAIN
+ARG PHPVERSION
+RUN apt update
+RUN apt -y upgrade
+RUN ln -snf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime && echo Asia/Jakarta > /etc/timezone
+RUN apt -y install iproute2 nano iputils-ping lsb-release gnupg2 ca-certificates apt-transport-https software-properties-common gcc make autoconf libc-dev pkg-config logrotate
+RUN add-apt-repository --yes ppa:ondrej/php
+RUN apt-get -y install apache2
+RUN echo "ServerName $CONTAINER_DOMAIN" | tee -a /etc/apache2/apache2.conf >/dev/null
+RUN apt -y install mariadb-client
+RUN apt -y install php$PHPVERSION php$PHPVERSION-mysql libapache2-mod-php$PHPVERSION php$PHPVERSION-cli php$PHPVERSION-cgi php$PHPVERSION-gd zip
+RUN a2enmod rewrite
+RUN phpenmod mbstring
+RUN apt -y install php$PHPVERSION-dev
+RUN apt -y install php$PHPVERSION-mbstring php$PHPVERSION-intl
+RUN apt -y install libmcrypt-dev
+RUN sed -i '6i\Listen 8080' /etc/apache2/ports.conf
+RUN echo '#!/bin/bash\n\n\nsource /etc/apache2/envvars\n#tail -F /var/log/apache2/* &\nexec apache2 -D FOREGROUND' > /boot/start.sh
+RUN chmod +x /boot/start.sh
+RUN apt -y install imagemagick imagemagick-doc
+RUN apt -y install php$PHPVERSION-Imagick
+RUN apt -y install php$PHPVERSION-intl
+RUN apt -y install git composer
+RUN sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+EXPOSE 80
+WORKDIR /var/www/html/
+CMD /boot/start.sh
---
+++ www/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>LAMP STACK</title>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
+</head>
+
+<body>
+    <section class='hero is-medium is-info is-bold'>
+        <div class='hero-body'>
+            <div class='container has-text-centered'>
+                <h1 class='title'>LAMP STACK</h1>
+                <h2 class='subtitle'>Your local development environment</h2>
+            </div>
+        </div>
+    </section>
+    <section class='section'>
+        <div class='container'>
+            <div class='columns'>
+                <div class='column'>
+                    <h3 class='title is-3 has-text-centered'>Environment</h3>
+                    <hr>
+                    <div class='content'>
+                        <ul>
+                            <li><?= apache_get_version(); ?></li>
+                            <li>PHP<?= phpversion(); ?></li>
+                            <li><?php
+                                try {
+                                    $link = @mysqli_connect('127.0.0.1', '', '', null);
+                                    if (mysqli_connect_errno()) {
+                                        throw new Exception('MySQL connection failed: ' . mysqli_connect_error());
+                                    } else {
+                                        printf('MySQL Server %s', mysqli_get_server_info($link));
+                                    }
+                                    mysqli_close($link);
+                                } catch (Exception $e) {
+                                    echo $e->getMessage();
+                                }
+                                ?></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class='column'>
+                    <h3 class='title is-3 has-text-centered'>Quick Links</h3>
+                    <hr>
+                    <div class='content'>
+                        <ul>
+                            <li><b><?php echo date('Y-m-d H:i:s'); ?></b></li>
+                            <li><a href='http://<?php echo $_SERVER['HTTP_HOST']; ?>/phpinfo.php'>phpinfo()</a></li>
+                            <li><a href='http://<?php echo $_SERVER['HTTP_HOST']; ?>:8080'>phpMyAdmin</a></li>
+                            <li><a href='http://<?php echo $_SERVER['HTTP_HOST']; ?>/test_db.php'>Test DB Connection</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
